41785bf08461933007bad181febfe458be28f917
nilness <nilness@gmai.com>
2013-09-15 11:55:50 -0500

    Initial classes & info file.

---
+++ common/letsmodmod/lib/Reference.java
@@ -0,0 +1,8 @@
+package letsmodmod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID ="LetsMod";
+    public static final String MOD_NAME = "Let's Mod";
+    public static final String VERSION = "0.0.1";
+}
